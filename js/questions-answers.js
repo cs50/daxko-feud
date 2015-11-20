625686e2f59333851a6ae20cd23d72c9d4273e3a
Colton Ogden <coltonoscopy@gmail.com>
2015-11-20 09:43:52 -0500

    replaced logo and changed questions

--- js/questions-answers.js
+++ js/questions-answers.js
@@ -1,20 +1,49 @@
 // questions must be less than 104 characters
 // answers must be less than 18 characters
 var data = [
-  ["Reason someone might make fun of your car", [
-    'Old', 29,
-    'Dirty', 23, 
-    'Color/Paint Job', 21, 
-    'Has dents/Beat up', 8, 
-    'Too small',5, 
-    'Too big', 3
+  ['A function declared in stdio.h', [
+    'printf', 68,
+    'scanf', 12,
+    'fopen', 10,
+    'fgetc', 5,
+    'fclose', 5
   ]],
-  ["Famous 'Martin'", [
-    'MLK JR', 25,
-    'Martin Lawrence', 22,
-    'Dean Martin', 19,
-    'Martin Sheen', 15,
-    'Steve Martin', 11,
-    'Martin Short', 8
+  ['Software used while programming', [
+    'Text Editor', 28,
+    'Compiler', 23,
+    'Debugger', 15,
+    'Facebook', 15,
+    'Browser', 13,
+    'Spotify', 6
+  ]],
+  ["I'm never gonna...", [
+    'Give you up', 28,
+    'Let you down', 16,
+    'Run around and desert you', 15,
+    'Make you cry', 15,
+    'Say goodbye', 14,
+    'Tell a lie and hurt you', 12
+  ]],
+  ['Favorite problem set', [
+    'pset7 / C$50 Finance', 27,
+    'pset0 / Scratch', 26,
+    'pset2 / Crypto', 25,
+    'pset3 / Game of Fifteen', 22
+  ]],
+  ['What would be the wrong way to go with sorting a list of a million 32-bit integers?', [
+    'Bubble Sort', 42,
+    'Insertion Sort', 21,
+    'Selection Sort', 14,
+    'Merge Sort', 12,
+    'Quicksort', 6,
+    'Gnome Sort', 5
+  ]],
+  ['With what browser did you submit Problem Set 8?', [
+    'Chrome', 81,
+    'Safari', 12,
+    'Firefox', 4,
+    'Edge', 1,
+    'Internet Explorer', 1,
+    'Safari iPhone', 1
   ]]
 ];
